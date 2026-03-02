d9bab836b5aa7bdc17c6ee9bf5ad4531bc252b3b
Nico Corrales <nicocorrales13@gmail.com>
2026-03-02 11:36:41 +0100

    CORRECION

--- src/js/supabaseClient.js
+++ src/js/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+const SUPABASE_URL = "https://cqnnxxcymelkwlcywokz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbm54eGN5bWVsa3dsY3l3b2t6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NzE1OTMsImV4cCI6MjA4NDU0NzU5M30.20Gv9evX-N5YdO0rNMMhj1RWw_wrpHV9CHpa3t2znxs";
+
 export const supabase = createClient(
-    "TU_SUPABASE_URL",
-    "TU_SUPABASE_ANON_KEY"
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
